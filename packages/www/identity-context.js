7c54292bec48877f8f8546db6909a30e11aa91c6
Petros Kyriakou <9pitops@gmail.com>
2020-02-12 21:17:40 +0400

    add netlify identity

---
+++ packages/www/identity-context.js
@@ -0,0 +1,28 @@
+const React = require('react')
+const netlifyIdentity = require('netlify-identity-widget')
+
+const IdentityContext = React.createContext({})
+
+exports.IdentityContext = IdentityContext
+const Provider = props => {
+  const [user, setUser] = React.useState()
+  React.useEffect(() => {
+    netlifyIdentity.init({})
+    netlifyIdentity.on('login', user => {
+      netlifyIdentity.close()
+      setUser(user)
+    })
+    netlifyIdentity.on('logout', () => {
+      netlifyIdentity.close()
+      setUser()
+    })
+  })
+
+  return (
+    <IdentityContext.Provider value={{ identity: netlifyIdentity, user }}>
+      {props.children}
+    </IdentityContext.Provider>
+  )
+}
+
+exports.Provider = Provider
--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,22 +1,34 @@
-import React, { useEffect } from 'react'
-import { Container, Heading, Button, Flex } from 'theme-ui'
-import netlifyIdentity from 'netlify-identity-widget'
-
+import React, { useContext } from 'react'
+import { Container, Heading, Button, Flex, NavLink } from 'theme-ui'
+import { Link } from 'gatsby'
+import { IdentityContext } from '../../identity-context'
 export default props => {
-  useEffect(() => {
-    netlifyIdentity.init({})
-  })
+  const { user, identity: netlifyIdentity } = useContext(IdentityContext)
+
   return (
     <Container>
+      <Flex as='nav'>
+        <NavLink as={Link} to='/' p={2}>
+          Home
+        </NavLink>
+        <NavLink as={Link} to={'/app'} p={2}>
+          Dashboard
+        </NavLink>
+        {user && (
+          <NavLink href='#!' p={2}>
+            {user.user_metadata.full_name}
+          </NavLink>
+        )}
+      </Flex>
       <Flex sx={{ flexDirection: 'column', padding: 3 }}>
-        <Heading as='h1'>Get stuff done</Heading>
+        <Heading as='h1'>Get Stuff Done</Heading>
         <Button
           sx={{ marginTop: 2 }}
           onClick={() => {
             netlifyIdentity.open()
           }}
         >
-          Log in
+          Log In
         </Button>
       </Flex>
     </Container>
